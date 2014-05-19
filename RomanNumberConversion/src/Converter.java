dd42a7ab9eb16466bb7c92ef9b21bbb819a28aeb
Aline <alinegsantoss@gmail.com>
2014-05-19 18:15:13 -0300

    adicionando projeto de conversao de romanos

---
+++ RomanNumberConversion/src/Converter.java
@@ -0,0 +1,72 @@
+public class Converter {
+
+private int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+public Converter(){}
+
+private int decodeRomanCharacter(char letter){
+switch(letter){
+case 'M': return 1000;
+case 'D': return 500;
+case 'C': return 100;
+case 'L': return 50;
+case 'X': return 10;
+case 'V': return 5;
+case 'I': return 1;
+default: return 0;
+}
+}
+
+public int romanToInt(String number) throws IllegalArgumentException{
+if(number.length() == 0 || !allCharactersInRange(number))
+throw new IllegalArgumentException();
+
+int result=0, i=0, maxSize = number.length();
+
+while(i < maxSize){
+if(((i+1) < maxSize) && (decodeRomanCharacter(number.charAt(i+1)) > decodeRomanCharacter(number.charAt(i)))){
+result += (decodeRomanCharacter(number.charAt(i+1)) - decodeRomanCharacter(number.charAt(i)));
+i+=2;
+}else{
+result += decodeRomanCharacter(number.charAt(i));
+i++;
+}
+
+}	
+
+return result;
+}
+
+private boolean allCharactersInRange(String number) {
+boolean ok;
+
+for(int i=0; i<number.length(); i++){
+ok = false;
+
+for(String character_roman : letters){
+if(Character.toString(number.charAt(i)).equals(character_roman)) ok = true;
+}
+
+if(!ok) return false;
+}
+
+return true;
+}
+
+public String intToRoman(int number) {
+if(number <= 0 || number >= 4000)
+throw new IllegalArgumentException();
+
+String result = "";
+
+for(int i=0; i<numbers.length; i++){
+while(number >= numbers[i]){
+result += letters[i];
+number -= numbers[i];
+}
+}
+
+return result;
+}
+}
---
+++ RomanNumberConversion/src/TestConverter.java
@@ -0,0 +1,74 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestConverter {
+
+private Converter converter;
+
+@Before
+public void setUp(){
+converter = new Converter();
+}
+
+@Test(expected=IllegalArgumentException.class)
+public void testRomanToIntEmptyString() {
+converter.romanToInt("");
+}
+
+@Test(expected=IllegalArgumentException.class)
+public void testRomanToIntNonRomanNumber() {
+converter.romanToInt("123");
+}
+
+@Test
+public void testConvertRomanToIntASimpleNumber(){
+assertEquals(10, converter.romanToInt("X"));
+assertEquals(1000, converter.romanToInt("M"));
+}
+
+@Test
+public void testConvertRomanToIntACompoundNumber(){
+assertEquals(16, converter.romanToInt("XVI"));
+assertEquals(22, converter.romanToInt("XXII"));
+}
+
+@Test
+public void testConvertRomanToIntASpecialNumber(){
+assertEquals(14, converter.romanToInt("XIV"));
+assertEquals(4, converter.romanToInt("IV"));
+}
+
+@Test(expected=IllegalArgumentException.class)
+public void testIntToRomanWithZeroArgument(){
+converter.intToRoman(0);
+}
+
+@Test(expected=IllegalArgumentException.class)
+public void testIntToRomanWithNegativeArgument(){
+converter.intToRoman(-5);
+}
+
+@Test(expected=IllegalArgumentException.class)
+public void testIntToRomanWithHighArgument(){
+converter.intToRoman(4000);
+}
+
+@Test
+public void testConvertIntToRomanASimpleNumber(){
+assertEquals("X", converter.intToRoman(10));
+}
+
+@Test
+public void testConvertIntToRomanACompoundNumber(){
+assertEquals("XXI", converter.intToRoman(21));
+assertEquals("MDCLXVI", converter.intToRoman(1666));
+}
+
+@Test
+public void testConvertIntToRomanASpecialNumber(){
+assertEquals("XIX", converter.intToRoman(19));
+}
+}
+
